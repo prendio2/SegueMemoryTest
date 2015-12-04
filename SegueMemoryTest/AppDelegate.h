24a235f960a35b19b9f5fca7daef1835b214c399
Oisin Prendiville <oisin@supertop.co>
2015-12-03 22:49:56 -0800

    Initial Commit

---
+++ SegueMemoryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegueMemoryTest
+//
+//  Created by Oisin Prendiville on 03/12/2015.
+//  Copyright © 2015 Oisin Prendiville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SegueMemoryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SegueMemoryTest
+//
+//  Created by Oisin Prendiville on 03/12/2015.
+//  Copyright © 2015 Oisin Prendiville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
